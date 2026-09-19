5b64dd01bafa1e2b4966f2fd80071c16b23003cf
Infnixmim572 <milly04warahmah@gmail.com>
2026-09-19 16:59:01 +0300

    Reorganize imports and update routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,54 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'sonner';
+import { UserAuthProvider } from '@/contexts/UserAuthContext';
 import Index from '@/pages/Index';
 import NotFound from '@/pages/NotFound';
 import LoginPage from '@/pages/user/LoginPage';
 import RegisterPage from '@/pages/user/RegisterPage';
 import HomePage from '@/pages/user/HomePage';
-import MinePage from '@/pages/user/MinePage';
 import PackagesPage from '@/pages/user/PackagesPage';
 import BuyPackagePage from '@/pages/user/BuyPackagePage';
-import MyProductsPage from '@/pages/user/MyProductsPage';
-import TeamPage from '@/pages/user/TeamPage';
-import WithdrawPage from '@/pages/user/WithdrawPage';
 import RechargePage from '@/pages/user/RechargePage';
+import WithdrawPage from '@/pages/user/WithdrawPage';
+import TeamPage from '@/pages/user/TeamPage';
+import MinePage from '@/pages/user/MinePage';
+import MyProductsPage from '@/pages/user/MyProductsPage';
 import RecordsPage from '@/pages/user/RecordsPage';
+import AboutUsPage from '@/pages/user/AboutUsPage';
 import RedeemPage from '@/pages/user/RedeemPage';
 import MissionCenterPage from '@/pages/user/MissionCenterPage';
-import AboutUsPage from '@/pages/user/AboutUsPage';
-import { UserAuthProvider } from '@/contexts/UserAuthContext';
 
-const App = () => {
+export default function App() {
   return (
-    <UserAuthProvider>
-      <BrowserRouter basename="/Infnix-mobile-9bugue-36088-20260919/">
+    <BrowserRouter basename="/Infnix-mobile-9bugue-36088-20260919/">
+      <UserAuthProvider>
+        <Toaster position="top-center" richColors />
         <Routes>
-          {/* Admin */}
+          {/* ADMIN now has its own link */}
           <Route path="/admin" element={<Index />} />
 
-          {/* User Auth */}
+          {/* HOMEPAGE is now Register */}
+          <Route path="/" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
 
-          {/* User Pages */}
-          <Route path="/" element={<HomePage />} />
-          <Route path="/mine" element={<MinePage />} />
+          {/* User App */}
+          <Route path="/home" element={<HomePage />} />
           <Route path="/packages" element={<PackagesPage />} />
-          <Route path="/buy/:packageId" element={<BuyPackagePage />} />
-          <Route path="/my-products" element={<MyProductsPage />} />
-          <Route path="/team" element={<TeamPage />} />
-          <Route path="/withdraw" element={<WithdrawPage />} />
+          <Route path="/buy/:productId" element={<BuyPackagePage />} />
           <Route path="/recharge" element={<RechargePage />} />
+          <Route path="/withdraw" element={<WithdrawPage />} />
+          <Route path="/team" element={<TeamPage />} />
+          <Route path="/mine" element={<MinePage />} />
+          <Route path="/my-products" element={<MyProductsPage />} />
           <Route path="/records" element={<RecordsPage />} />
+          <Route path="/about" element={<AboutUsPage />} />
           <Route path="/redeem" element={<RedeemPage />} />
           <Route path="/missions" element={<MissionCenterPage />} />
-          <Route path="/about" element={<AboutUsPage />} />
 
-          {/* 404 */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </UserAuthProvider>
+      </UserAuthProvider>
+    </BrowserRouter>
   );
-};
-
-export default App;
+}
