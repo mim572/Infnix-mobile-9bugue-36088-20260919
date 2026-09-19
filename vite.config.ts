cdd6068820935cd58b68c3a3101a4a59d97e5259
Infnixmim572 <milly04warahmah@gmail.com>
2026-09-19 22:53:38 +0300

    Initialize Vite configuration with React and server settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,20 @@
-b
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/", // FIXED - must be / for mim572.github.io
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
